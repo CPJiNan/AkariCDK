529ea703d2740284b131d6a89a5e22e3667fa13d
季楠 <2759278070@qq.com>
2024-11-03 13:16:44 +0800

    [3.1.5] [+]升级 Taboolib 版本至 6.2.0-beta33

--- src/main/kotlin/com/github/cpjinan/plugin/akaricdk/internal/command/subCommand/CodeCommand.kt
+++ src/main/kotlin/com/github/cpjinan/plugin/akaricdk/internal/command/subCommand/CodeCommand.kt
@@ -5,25 +5,11 @@ import taboolib.common.platform.ProxyCommandSender
 import taboolib.common.platform.command.subCommand
 import taboolib.expansion.createHelper
 import taboolib.module.lang.sendLang
-import java.util.*
 
 object CodeCommand {
     val code = subCommand {
         createHelper()
         literal("create") {
-            literal("randomCode").dynamic("kit").dynamic("isServerWide").dynamic("amount") {
-                execute<ProxyCommandSender> { sender, context, _ ->
-                    val randomList = List(context["amount"].toInt()) { UUID.randomUUID().toString() }
-                    val codeList = randomList.forEach {
-                        ConfigManager.getCodeList().toMutableList().apply {
-                            add("${it}|${context["kit"]}|${context["isServerWide"]}")
-                        }
-                    }
-                    ConfigManager.code["Redeem-Code"] = codeList
-                    ConfigManager.code.saveToFile()
-                    sender.sendLang("Create-Code", context["code"])
-                }
-            }
             dynamic("code").dynamic("kit").dynamic("isServerWide") {
                 execute<ProxyCommandSender> { sender, context, _ ->
                     val codeList = ConfigManager.getCodeList().toMutableList().apply {
